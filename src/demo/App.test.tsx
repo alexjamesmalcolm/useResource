89100eb1826005b27a1843ae7e0875bf1dc6c93a
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-06-18 20:11:29 -0400

    Fixed App.test.tsx

--- src/demo/App.test.tsx
+++ src/demo/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as useResource from "../lib";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const useResourceSpy = jest.spyOn(useResource, "default");
+  useResourceSpy.mockReturnValue({
+    actions: { getResource: jest.fn() },
+    data: null,
+    isLoading: true,
+    error: null,
+    filterCache: jest.fn(),
+    isInStore: false,
+  });
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
