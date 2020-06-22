3be24e672c92ad1eb7432297835bc4eaddf70671
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-06-22 11:33:05 -0400

    Prefixed actions to reduce disk size

--- src/lib/hooks/useResource/actionTypes.ts
+++ src/lib/hooks/useResource/actionTypes.ts
@@ -1,4 +1,5 @@
-export const REQUEST_INITIAL = "useResource_REQUEST_INITIAL";
-export const REQUEST_SUCCESS = "useResource_REQUEST_SUCCESS";
-export const REQUEST_FAILURE = "useResource_REQUEST_FAILURE";
-export const CLEAR_CACHED_RESOURCE = "useResource_CLEAR_CACHED_RESOURCE";
+const p = "useResource";
+export const REQUEST_INITIAL = `${p}_REQUEST_INITIAL`;
+export const REQUEST_SUCCESS = `${p}_REQUEST_SUCCESS`;
+export const REQUEST_FAILURE = `${p}_REQUEST_FAILURE`;
+export const CLEAR_CACHED_RESOURCE = `${p}_CLEAR_CACHED_RESOURCE`;
