1d0e822da2c05b62e08d811964c3d4f494317e0f
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-14 17:37:12 -0400

    Created test to prevent overfetching

--- src/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import { reducer as useResource } from "./lib";
-
-const rootReducer = combineReducers({ useResource });
-
-const store = createStore(rootReducer);
-
-export default store;
