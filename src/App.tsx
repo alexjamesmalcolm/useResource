bb4cab95c1e981f188719b39baf75740f9278240
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-15 14:18:05 -0400

    Modified App.tsx for what I'm testing for

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
 import React, { useCallback } from "react";
 import useResource from "./lib";
-import Setup from "./examples/Setup";
 
 const App = () => {
-  // const getResourceId = useCallback(() => {
-  //   return "resource-id";
-  // }, []);
-  // const getResource = useCallback(
-  //   () =>
-  //     fetch("http://www.asterank.com/api/mpc?limit=100").then((response) =>
-  //       response.json()
-  //     ),
-  //   []
-  // );
-  // const { isLoading, data } = useResource(getResourceId, { getResource });
-  // if (isLoading || !data) return <p>Loading...</p>;
-  // console.log(data);
-  const getResource = useCallback(() => Promise.resolve(1), []);
-  const doNothing = useCallback(() => Promise.resolve(), []);
-  const {
-    data,
-    actions: { doNothing: a, getResource: b },
-  } = useResource("hi", { getResource, doNothing });
-  return (
-    <p>{data}</p>
-    // <Setup />
-    // <div>
-    //   <pre>
-    //     {data.map((planet) => (
-    //       <p key={planet.readable_des}>{planet.readable_des}</p>
-    //     ))}
-    //   </pre>
-    // </div>
-  );
+  const resourceId = "resourceId";
+  const getResource = useCallback(() => {
+    console.log("getResource has been called");
+    return new Promise((resolve) => {
+      setTimeout(() => resolve("Hello there"), 1000);
+    });
+  }, []);
+  useResource(resourceId, { getResource });
+  useResource(resourceId, { getResource });
+  return <p>Hi</p>;
 };
 
 export default App;
