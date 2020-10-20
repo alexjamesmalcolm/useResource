f69ffd83aff07b9f9671f2696d57a8105ec138ff
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-20 19:54:54 -0400

    Created types for serializer and deserializer

--- src/lib/useResource/index.ts
+++ src/lib/useResource/index.ts
@@ -6,6 +6,12 @@ import useGetterActionWithCache from "./useGetterActionWithCache";
 import useTransformativeActionsWithCache from "./useTransformativeActionsWithCache";
 import useAcquireEffect from "./useAcquireEffect";
 
+type Primitive = number | boolean | string;
+type SerializableObject = {
+  [key in number | string]: Primitive;
+};
+type Serializable = SerializableObject | SerializableObject[] | Primitive;
+
 interface UseResourceResponse<T, A extends OtherActions<T>> {
   actions: { getResource: () => Promise<T> } & A;
   isLoading: boolean;
@@ -15,10 +21,21 @@ interface UseResourceResponse<T, A extends OtherActions<T>> {
   filterCache: (filterCallback: FilterCallback) => void;
 }
 
-const useResource = <T extends unknown, A extends OtherActions<T>>(
+const useResource = <
+  T extends unknown,
+  A extends OtherActions<T>,
+  S extends Serializable
+>(
   getResourceId: string | (() => string),
   actions: { getResource: () => Promise<T> } & A,
-  options: { acquireImmediately?: boolean; ttl?: number | TtlCallback } = {}
+  options: {
+    acquireImmediately?: boolean;
+    ttl?: number | TtlCallback;
+    serialization?: {
+      serializer: (data: T) => S;
+      deserializer: (serializedData: S) => T;
+    };
+  } = {}
 ): UseResourceResponse<T, A> => {
   const { getResource } = actions;
   const { acquireImmediately = true, ttl = 0 } = options;
